7e8fda2141680fba4404feeea2238f3111354254
gabrielrapaport <gabrielrapaport@gmail.com>
2020-03-06 10:21:13 -0500

    added ft_memchr need to test

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gabrielrapaport <gabrielrapaport@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 09:51:54 by gabrielrapa       #+#    #+#             */
-/*   Updated: 2020/03/06 10:08:17 by gabrielrapa      ###   ########.fr       */
+/*   Updated: 2020/03/06 10:20:49 by gabrielrapa      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void    bft_zero(void *s, size_t n);
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void    *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
-
+void    *ft_memchr(const void *s, int c, size_t n);
 
 
 #endif
